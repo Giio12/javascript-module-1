3861d3ffaa775f1b95b6c9bb7a3bc3777c3afaaf
gio <toiigmar@gmail.com>
2021-06-29 19:04:40 +0200

    Segundo Ejercicio Hola mundo en 5 idiomas diferentes

---
+++ week-1/InClass/exercise-B.js
@@ -0,0 +1,7 @@
+console.log("Halo, dunia!") // Indonesian
+console.log("Ciao, mondo!") //Italian
+console.log("Hola, mundo!") // Spanish
+console.log("Hello world!") // English
+console.log("salut monde!") // French
+
+   
