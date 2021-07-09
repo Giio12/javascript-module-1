fc48e487d3d534479a2e96c9e7a0a2d5c3c33d55
gio <toiigmar@gmail.com>
2021-07-09 23:08:36 +0200

    Exercise F-logical-operators// Terminado

--- week-2/Homework/F-logical-operators/exercise.js
+++ week-2/Homework/F-logical-operators/exercise.js
@@ -9,6 +9,22 @@
 var htmlLevel = 8;
 var cssLevel = 4;
 
+function greaterThan(htmlLevel) {
+  if (htmlLevel > 5) {
+    return htmlLevelAbove5 = true;
+  }
+  return htmlLevelAbove5 = false;
+}
+greaterThan(htmlLevel);
+
+function greaterThan5(cssLevel) {
+  if (cssLevel > 5) {
+    return cssLevelAbove5 = true;
+  }
+  return cssLevelAbove5 = false;
+
+}
+greaterThan5(cssLevel)
 // Finish the statement to check whether HTML, CSS knowledge are above 5
 // (hint: use the comparison operator from before)
 var htmlLevelAbove5;
@@ -17,6 +33,22 @@ var cssLevelAbove5;
 // Finish the next two statement
 // Use the previous variables and logical operators
 // Do not "hardcode" the answers
+function compareIfBoth(htmlLevel, cssLevel) {
+  if (htmlLevel > 5 && cssLevel > 5) {
+    return cssAndHtmlAbove5 = true;
+  }
+  return cssAndHtmlAbove5 = false;
+}
+compareIfBoth(htmlLevel, cssLevel)
+
+function compareIfAny(htmlLevel, cssLevel) {
+  if (htmlLevel > 5 || cssLevel > 5) {
+    return cssOrHtmlAbove5 = true;
+  }
+  return cssOrHtmlAbove5 = false;
+}
+compareIfAny(htmlLevel, cssLevel)
+
 var cssAndHtmlAbove5;
 var cssOrHtmlAbove5;
 
