27b1f72bc2dbacc74c287eadf5ff36e7e511b89f
gio <toiigmar@gmail.com>
2021-07-22 22:09:51 +0200

    Exercise H:1 week2// Arreglado

--- week-2/Homework/H-array-literals/exercise.js
+++ week-2/Homework/H-array-literals/exercise.js
@@ -4,9 +4,10 @@
   Declare some variables assigned to arrays of values
 */
 
-var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; // add numbers from 1 to 10 into this array
-var mentors = ["Daniel", "Irina", "Rares"]; // Create an array with the names of the mentors: Daniel, Irina and Rares
-
+var numbers = []; // add numbers from 1 to 10 into this array
+var mentors = []; // Create an array with the names of the mentors: Daniel, Irina and Rares
+var newNumbers= numbers.push(1,2,3,4,5,6,7,8,9,10)
+var newMentors= mentors.push("Daniel", "Irina", "Rares")
 /* 
   DO NOT EDIT BELOW THIS LINE
   --------------------------- */
